e42904d225c75ee9a262612ae067f6925cde4b5d
Reagan Jones <112424803+RayJones01@users.noreply.github.com>
2026-03-03 22:40:26 -0600

    Create strings-basics.py

---
+++ strings-basics.py
@@ -0,0 +1,25 @@
+# Challenge: Strings Basics
+
+'''
+Instructions-
+1. Create a variable txt with the value "Hello, World!"
+2. Print the characters from index 2 to 5 (slicing)
+3. Print txt converted to upper case
+4. Create a variable name with the value "Python"
+5. Use an f-string to print "I love Python" using the name variable
+'''
+
+# Create the variable
+txt = "Hello, World!"
+
+# Print characters from index 2 to 5
+print(txt[2:5])
+
+# Print in upper case
+print(txt.upper())
+
+# Create the name variable
+name = "Python"
+
+# Print using an f-string
+print(f"I love {name}")
