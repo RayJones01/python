52aa95689c7eb9326af68d07dd96150b0bbfaa09
Reagan Jones <112424803+RayJones01@users.noreply.github.com>
2026-03-03 22:45:08 -0600

    Update strings-basics.py

--- strings-basics.py
+++ strings-basics.py
@@ -2,21 +2,21 @@
 
 '''
 Instructions-
-1. Create a variable txt with the value "Hello, World!"
+1. Create a variable with the value "Hello, World!"
 2. Print the characters from index 2 to 5 (slicing)
-3. Print txt converted to upper case
+3. Print variable converted to upper case
 4. Create a variable name with the value "Python"
 5. Use an f-string to print "I love Python" using the name variable
 '''
 
 # Create the variable
-txt = "Hello, World!"
+greet = "Hello, World!"
 
 # Print characters from index 2 to 5
-print(txt[2:5])
+print(greet[2:5])
 
 # Print in upper case
-print(txt.upper())
+print(greet.upper())
 
 # Create the name variable
 name = "Python"
