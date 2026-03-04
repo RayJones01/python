85c94c9cd9a6f40b2004f79ad1c74e6cb8391579
Reagan Jones <112424803+RayJones01@users.noreply.github.com>
2026-03-03 22:57:25 -0600

    Create lists-basics.py

---
+++ lists-basics.py
@@ -0,0 +1,29 @@
+# Challenge: Lists Basics
+
+'''
+Instructions-
+1. Create a list called colors with the values "red", "green", "blue"
+2. Print the first item in the list
+3. Change the second item to "yellow"
+4. Add "purple" to the end of the list
+5. Remove "red" from the list
+6. Print the list
+'''
+
+# Create a list
+colors = ["red", "green", "blue"]
+
+# Print the first item
+print(colors[0])
+
+# Change the second item to "yellow"
+colors[1] = "yellow"
+
+# Add "purple" to the end
+colors.append("purple")
+
+# Remove "red"
+colors.remove("red")
+
+# Print the list
+print(colors)
