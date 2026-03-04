94d5d571fa81ba8cd61180f15e003b5515d26601
Reagan Jones <112424803+RayJones01@users.noreply.github.com>
2026-03-03 22:50:52 -0600

    Create operators-basics.py

---
+++ operators-basics.py
@@ -0,0 +1,26 @@
+# Challenge: Operators Basics
+
+'''
+Instructions-
+1. Create two variables a = 15 and b = 4
+2. Print the result of a modulus b
+3. Print the result of a floor division b
+4. Print the result of a to the power of b
+5. Use an assignment operator to add 10 to a
+'''
+
+# Create variables
+a = 15
+b = 4
+
+# Print modulus
+print(a % b)
+
+# Print floor division
+print(a // b)
+
+# Print power
+print(a ** b)
+
+# Add 10 to a
+a += 10
