5360191a1037ee166d651e2e559865920209ae11
Reagan Jones <112424803+RayJones01@users.noreply.github.com>
2026-03-03 22:44:03 -0600

    Create type-casting.py

---
+++ type-casting.py
@@ -0,0 +1,22 @@
+# Challenge: Casting
+
+'''
+Instructions
+Create a variable with the integer value 1
+Convert to a float and store it in a
+Convert to a string and store it in b
+Print a and b
+'''
+
+# Create an integer
+x = 1
+
+# Convert to float
+a = float(x)
+
+# Convert to string
+b = str(x)
+
+# Print values
+print(a)
+print(b)
